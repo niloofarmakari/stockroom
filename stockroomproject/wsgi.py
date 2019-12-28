3c851b4c0294df8e0176f0e9d9d5f7ef967a6212
niloofar makari <niloofar_7_7@yahoo.com>
2019-12-28 14:37:45 +0330

    change the name of project

--- stockroomproject/wsgi.py
+++ stockroomproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stockroom project.
+WSGI config for stockroomproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockroom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockroomproject.settings')
 
 application = get_wsgi_application()
